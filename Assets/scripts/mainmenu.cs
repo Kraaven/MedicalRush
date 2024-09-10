fdff1de73a3d19bc858e5db3b23dafc23edcad4e
Samarth Ale <161304725+Ale1as@users.noreply.github.com>
2024-09-10 14:28:16 +0530

    main menu made
    -basic main menu
    -need art for main menu

---
+++ Assets/scripts/mainmenu.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenu : MonoBehaviour
+{
+    public GameObject mainMenu;
+
+    public GameObject aboutMenu;
+
+    public GameObject levelMenu;
+// This method loads the game scene, replace "GameScene" with your actual scene name
+    public void PlayGame()
+    {
+        mainMenu.SetActive(false);
+        levelMenu.SetActive(true);
+    }
+
+    // This method can be expanded to open a settings menu
+    public void About()
+    {
+        // Code to open the settings menu
+        aboutMenu.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+
+    // This method quits the game
+    public void QuitGame()
+    {
+        Debug.Log("Quit button pressed");
+        Application.Quit();
+    }
+
+    public void aboutBack()
+    {
+        aboutMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void levelBack()
+    {
+        mainMenu.SetActive(true);
+        levelMenu.SetActive(false);
+    }
+
+    public void level1()
+    {
+        print("level 1 started");
+    }
+    public void level2()
+    {
+        print("level 1 started");
+    }
+    public void level3()
+    {
+        print("level 1 started");
+    }
+
+}
