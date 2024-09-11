9b0208b315530f25e6be8a556b429528c4cf491d
Samarth Ale <161304725+Ale1as@users.noreply.github.com>
2024-09-11 22:20:40 +0530

    programming
    - tweaking in time keeper

--- Assets/scripts/Asmit Scripts/TimeKeeper.cs
+++ Assets/scripts/Asmit Scripts/TimeKeeper.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TimeKeeper : MonoBehaviour
 {
+    public ObjectCheckScript Ocs;
+    public List<GameObject> Equiped;
     private void Start()
     {
         StartCoroutine(CalmPhaseTimer());
@@ -12,7 +16,15 @@ public class TimeKeeper : MonoBehaviour
     IEnumerator CalmPhaseTimer()
     {
         yield return new WaitForSeconds(5);
-
-        Debug.Log("TIME UP");
+        if (Ocs.allEquip.SequenceEqual(Equiped))
+        {
+            Debug.Log("collected all equipment");
+        }
+        else
+        {
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            Debug.Log("TIME UP");
+        }
+        
     }
 }
--- Assets/scripts/samarth scripts/ObjectCheckScript.cs
+++ Assets/scripts/samarth scripts/ObjectCheckScript.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ObjectCheckScript : MonoBehaviour
 {
     public List<GameObject> basicEquip;
     public List<GameObject> specialEquip;
+    public List<GameObject> allEquip;
     
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,7 @@ public class ObjectCheckScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        allEquip = new List<GameObject>(basicEquip);
+        allEquip.AddRange(specialEquip);
     }
 }
